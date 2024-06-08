ed7213b67782fa52106d6bbec8247256f621831d
hazim.hasnan <hazimhasnan@gmail.com>
2024-06-08 21:24:26 +0800

    Add spring boot app

---
+++ src/main/java/com/hazimhasnan/zhealth/ZhealthApplication.java
@@ -0,0 +1,13 @@
+package com.hazimhasnan.zhealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhealthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hazimhasnan/zhealth/ZhealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hazimhasnan.zhealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
